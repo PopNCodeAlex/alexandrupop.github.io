b17487469586c6e81a5d5c927352dd91c2303f0c
Alexandru Pop <alex99_pop@yahoo.com>
2024-02-22 13:02:58 +0200

    added images on hobbyhorse page, added carousel, created energy app page

---
+++ assets/js/carousel.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function moveSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  let slides = document.getElementsByClassName("slide");
+  
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  
+  slides[slideIndex].classList.add("active");
+}
